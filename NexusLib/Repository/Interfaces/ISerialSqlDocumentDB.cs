ec25d068544cd0eba5b1ffd32594bba250fe6648
Grzegorz Brelski <brelski.grzegorz@gmail.com>
2018-06-16 20:21:03 +0200

    Add few serial operations Changes :  # Add :   @Collections:     * Create     * Delete   @Documents:     * Create     * Delete     * Read * UpdateDocuments

--- NexusLib/Repository/Interfaces/ISerialSqlDocumentDB.cs
+++ NexusLib/Repository/Interfaces/ISerialSqlDocumentDB.cs
@@ -1,11 +1,18 @@
-﻿using System;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using NexusLib.Model;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace NexusLib.Repository.Interfaces
 {
     public interface ISerialSqlDocumentDB
     {
+        IList<Task<BaseResponse<DocumentCollection>>> CreateCollections(IEnumerable<(string collectionId, string partitionKeyPath, string databaseID, RequestOptions requestOptions)> collectionParams);
+        IList<Task<BaseResponse<DocumentCollection>>> DeleteCollections(IEnumerable<(string databaseID, string collectionID)> collectionParams);
+        IList<Task<BaseResponse<Document>>> CreateDocuments(IEnumerable<(object document, string collectionID, string databaseID)> documentsParams);
+        IList<Task<BaseResponse<Document>>> DeleteDocuments(IEnumerable<(string documentID, string collectionID, string databaseID)> documentsParams);
+        IList<Task<BaseResponse<Document>>> ReadDocuments(IEnumerable<(string documentID, string partitionKey, string colId, string dbID)> documentParams);
+        IList<Task<BaseResponse<Document>>> UpdateDocuments<DocumentType>(IEnumerable<(string documentID, DocumentType document, string colId, string dbID)> documentParams);
     }
 }
---
+++ NexusLib/Repository/SerialSqlDocumentDB.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using NexusLib.Model;
+using NexusLib.Repository.Interfaces;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NexusLib.Repository
+{
+    public class SerialSqlDocumentDB : ISerialSqlDocumentDB
+    {
+        private ISqlDocumentsDB sqlDocumentsDB;
+        public SerialSqlDocumentDB(ISqlDocumentsDB sqlDocumentsDB)
+        {
+            this.sqlDocumentsDB = sqlDocumentsDB;
+        }
+
+        public IList<Task<BaseResponse<DocumentCollection>>> CreateCollections(IEnumerable<(string collectionId, string partitionKeyPath, string databaseID, RequestOptions requestOptions)> collectionParams)
+            => collectionParams.Select(x => sqlDocumentsDB.CreateCollection(x.collectionId, x.partitionKeyPath, x.databaseID, x.requestOptions)).ToList();
+
+        public IList<Task<BaseResponse<DocumentCollection>>> DeleteCollections(IEnumerable<(string databaseID, string collectionID)> collectionParams) 
+            => collectionParams.Select(x => sqlDocumentsDB.DeleteCollection(x.databaseID, x.collectionID)).ToList();
+
+        public IList<Task<BaseResponse<Document>>> CreateDocuments(IEnumerable<(object document, string collectionID, string databaseID)> documentsParams)
+            => documentsParams.Select(x => sqlDocumentsDB.CreateDocumentAsync(x.document, x.collectionID, x.databaseID)).ToList();
+
+        public IList<Task<BaseResponse<Document>>> DeleteDocuments(IEnumerable<(string documentID, string collectionID , string databaseID)> documentsParams)
+            => documentsParams.Select(x => sqlDocumentsDB.DeleteDocumentAsync(x.documentID, x.collectionID, x.databaseID)).ToList();
+
+        public IList<Task<BaseResponse<Document>>> ReadDocuments(IEnumerable<(string documentID, string partitionKey, string colId, string dbID)> documentParams)
+            => documentParams.Select(x => sqlDocumentsDB.ReadDocumentAsync(x.documentID, x.partitionKey, x.colId, x.dbID)).ToList();
+
+        public IList<Task<BaseResponse<Document>>> UpdateDocuments<DocumentType>(IEnumerable<(string documentID, DocumentType document, string colId, string dbID)> documentParams)
+            => documentParams.Select(x => sqlDocumentsDB.UpdateDocumentAsync(x.documentID, x.document, x.colId, x.dbID)).ToList();
+    }
+}
