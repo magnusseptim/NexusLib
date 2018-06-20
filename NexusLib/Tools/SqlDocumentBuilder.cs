39bf1f775b8eaea8f31da03845878602c1548156
Grzegorz Brelski <brelski.grzegorz@gmail.com>
2018-06-20 20:24:40 +0200

    Allow to add preffered locations for SqlDocumentBuilder and build DocumentClient with them.

--- NexusLib/Tools/SqlDocumentBuilder.cs
+++ NexusLib/Tools/SqlDocumentBuilder.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.Documents.Client;
+﻿using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
 using NexusLib.Model;
 using NexusLib.Repository;
 using NexusLib.Tools.Interfaces;
@@ -17,19 +18,20 @@ namespace NexusLib.Tools
         private string endpointUrl;
         private string primaryKey;
         private int offerThroughput;
+        private List<string> prefferedLocations;
 
+        // TODO : Test this !!!
         public SqlDocumentsDB Return()
         {
             buildedObject.stdInvocator = standardInvocator;
             buildedObject.logger = logger;
-            buildedObject.Client = new DocumentClient(new Uri(endpointUrl), primaryKey);
+            buildedObject.Client = new DocumentClient(new Uri(endpointUrl), primaryKey, BuildPrefferedLocationsPolicy(prefferedLocations));
             buildedObject.Databases = new HashSet<DatabaseSchema>();
             buildedObject.ActiveDatabaseID = Vault.DefaultActiveDBID;
             buildedObject.ActiveCollection = Vault.DefaultActiveColID;
             buildedObject.DefaultRequestOptrions = new RequestOptions() { OfferThroughput = offerThroughput };
             return buildedObject;
         }
-
         public static SqlDocumentBuilder BuildBareObject()
         {
             return new SqlDocumentBuilder();
@@ -65,6 +67,22 @@ namespace NexusLib.Tools
             return this;
         }
 
+        public SqlDocumentBuilder PrefferedLocations(List<string> prefferedLocations)
+        {
+            this.prefferedLocations = prefferedLocations;
+            return this;
+        }
 
+        /// <summary>
+        /// Allow to create connection policy with preffered locations based on parameter
+        /// </summary>
+        /// <param name="prefLocations">Preffered locations as string</param>
+        /// <returns>Connection Policy</returns>
+        private ConnectionPolicy BuildPrefferedLocationsPolicy(List<string> prefLocations)
+        {
+            var conPolicy = new ConnectionPolicy();
+            prefferedLocations.ForEach(x => conPolicy.PreferredLocations.Add(x));
+            return conPolicy;
+        }     
     }
 }
